491aa9712faea3742217acd8a293cf4e21ae559d
Low Kian Seong <kianseong@gmail.com>
2014-07-06 10:07:51 +0800

    Adding metadata to setup.py and minor pep8 cleanup

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version='0.0.1',
     author='Park Sung Min',
     author_email='vamf12@gmail.com',
+    description='Python to JavaScript. Coroutine support with streamlinejs.',
+    url='https://github.com/vamf12/streamlinepy',
     packages=['streamline'],
     install_requires=['jinja2'],
     test_suite='nose.collector',
